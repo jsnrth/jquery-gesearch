50080fd9672dc3e8c047ff23a2fe3f4040b9cc6c
Jason <jasonmichaelroth@gmail.com>
2011-02-12 10:02:57 -0700

    The starting point

---
+++ README.md
@@ -0,0 +1,4 @@
+Google Earth Search jQuery Plugin
+=================================
+
+This plugin enables geo-coded searches in the Google Earth browser plugin using the google maps v3 geo coding API.
---
+++ jquery.gesearch.js
@@ -0,0 +1,10 @@
+/*!
+ * Google Earth Search jQuery Plugin
+ *
+ * Copyright 2011, Jason Roth
+ * Artistic License 2.0
+ * http://www.opensource.org/licenses/artistic-license-2.0
+ *
+ * Date: Sat Feb 12 09:47:06 2011 -0700
+**/
+
